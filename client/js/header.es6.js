d8202737fb5a20859c29462af0fc98786670d9e9
benji6 <benhall22@hotmail.co.uk>
2015-05-28 16:47:29 +0100

    less hacky way of highlighting active nav buttons

--- client/js/header.es6.js
+++ client/js/header.es6.js
@@ -1,8 +1,25 @@
 Template.header.helpers({
   navItems: () => [
-    {name: "Enter Transactions", path: "enter_transactions", selected: Session.get("enter_transactions_selected")},
-    {name: "View Transactions", path: "view_transactions", selected: Session.get("view_transactions_selected")},
-    {name: "Set Budget", path: "set_budget", selected: Session.get("set_budget_selected")},
-    {name: "View Performance", path: "view_performance", selected: Session.get("view_performance_selected")},
+    {
+      name: "Enter Transactions",
+      path: "enter_transactions",
+      selected: Router.current().route.path() === "/enter_transactions" ||
+        Router.current().route.path() ===  "/",
+    },
+    {
+      name: "View Transactions",
+      path: "view_transactions",
+      selected: Router.current().route.path() === "/view_transactions",
+    },
+    {
+      name: "Set Budget",
+      path: "set_budget",
+      selected: Router.current().route.path() === "/set_budget",
+    },
+    {
+      name: "View Performance",
+      path: "view_performance",
+      selected: Router.current().route.path() === "/view_performance",
+    },
   ]
 });
