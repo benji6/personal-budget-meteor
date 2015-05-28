068b4ce04a568a86032811d53c931d416d3ad00e
benji6 <benhall22@hotmail.co.uk>
2015-05-28 15:57:35 +0100

    much nicer navigation

--- client/client.es6.js
+++ client/client.es6.js
@@ -15,6 +15,15 @@ Template.enter_transactions.helpers({
   mostRecentTransactions: () => Transactions.find({}, {sort: {date: -1}, limit: 5}),
 });
 
+Template.header.helpers({
+  navItems: () => [
+    {name: "Enter Transactions", path: "enter_transactions", selected: Session.get("enter_transactions_selected")},
+    {name: "Set Budget", path: "set_budget", selected: Session.get("set_budget_selected")},
+    {name: "View Performance", path: "view_performance", selected: Session.get("view_performance_selected")},
+    {name: "View Summary", path: "view_summary", selected: Session.get("view_summary_selected")},
+  ]
+});
+
 Template.enter_transactions.rendered = () => $(".datepicker").datepicker({dateFormat: "dd/mm/yy"});
 
 Template.transaction.events({
